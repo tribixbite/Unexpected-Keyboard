a7cf4d839f0d404bd29ec9a3d4c5612b797b2f9f
tribixbite <willstone@gmail.com>
2025-11-26 01:10:19 -0500

    refactor: Migrate EnhancedSwipeGestureRecognizer.java to Kotlin
    - EnhancedSwipeGestureRecognizer.java (14 lines) → EnhancedSwipeGestureRecognizer.kt (7 lines)
    - Simple wrapper class using Kotlin inheritance syntax
    - One-line class declaration
    
    Migration 18/98 complete

--- srcs/juloo.keyboard2/EnhancedSwipeGestureRecognizer.kt
+++ srcs/juloo.keyboard2/EnhancedSwipeGestureRecognizer.kt
@@ -1,14 +1,8 @@
-package juloo.keyboard2;
+package juloo.keyboard2
 
 /**
  * Enhanced Swipe Gesture Recognizer
  * Simple wrapper around ImprovedSwipeGestureRecognizer
  * CGR-based prediction code removed - neural system handles all predictions
  */
-public class EnhancedSwipeGestureRecognizer extends ImprovedSwipeGestureRecognizer
-{
-  public EnhancedSwipeGestureRecognizer()
-  {
-    super();
-  }
-}
+class EnhancedSwipeGestureRecognizer : ImprovedSwipeGestureRecognizer()
